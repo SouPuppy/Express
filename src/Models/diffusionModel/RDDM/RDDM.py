23fca7758ae341842e60df6728d9f7fe0bd5f8c9
soupup <wdk1745@gmail.com>
2024-11-23 17:04:44 +0800

    init

--- src/Models/diffusionModel/RDDM/RDDM.py
+++ src/Models/diffusionModel/RDDM/RDDM.py
@@ -11,27 +11,27 @@ class RDDM(CLASS_DIFFUSION):
         )
 
     def __call__(self, *args, **kwargs):
-        log("using [RDDM] diffusion model")
+    #     log("using [RDDM] diffusion model")
 
-        model = UnetRes(
-            dim=64,
-            dim_mults=(1, 2, 4, 8),
-            share_encoder=0,
-            condition=condition,
-            input_condition=input_condition
-        )
-        diffusion = ResidualDiffusion(
-            model,
-            image_size=image_size,
-            timesteps=1000,           # number of steps
-            # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
-            sampling_timesteps=5,
-            objective='pred_res_noise',
-            loss_type='l1',            # L1 or L2
-            condition=condition,
-            sum_scale = sum_scale,
-            input_condition=input_condition,
-            input_condition_mask=input_condition_mask
-        )
+    #     model = UnetRes(
+    #         dim=64,
+    #         dim_mults=(1, 2, 4, 8),
+    #         share_encoder=0,
+    #         condition=condition,
+    #         input_condition=input_condition
+    #     )
+    #     diffusion = ResidualDiffusion(
+    #         model,
+    #         image_size=image_size,
+    #         timesteps=1000,           # number of steps
+    #         # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
+    #         sampling_timesteps=5,
+    #         objective='pred_res_noise',
+    #         loss_type='l1',            # L1 or L2
+    #         condition=condition,
+    #         sum_scale = sum_scale,
+    #         input_condition=input_condition,
+    #         input_condition_mask=input_condition_mask
+    #     )
 
         return RDDM(*args, **kwargs)
